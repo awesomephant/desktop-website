850ced5a364c0f9d9216bb2ce2eebc19c28bc5db
Max Koehler <max@koehler-kn.de>
2017-06-03 17:53:17 +0100

    add leading zero to clock display

--- js/src/clock.js
+++ js/src/clock.js
@@ -1,8 +1,13 @@
+function padTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+
 var setClock = function(){
 	var clockEl = document.getElementById('clock')
 	var now = new Date();
-   var h = now.getHours();
-   var m = now.getMinutes();
+   var h = padTime(now.getHours());
+   var m = padTime(now.getMinutes());
 	clockEl.innerHTML = h + ':' + m;
 }
 setClock();
