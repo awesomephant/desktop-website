ce84d4c0cae81cdacbf2d8c7fb0ec12b40c84b34
Max Koehler <max@koehler-kn.de>
2017-06-05 18:43:53 +0100

    Move up function declaration

--- js/src/options-button.js
+++ js/src/options-button.js
@@ -1,8 +1,8 @@
+var toggleMenu = function(){
+	this.classList.toggle('menu-isOpen')
+}
+
 var tabs = document.querySelectorAll('.desktop-nav-item');
 for (var i = 0; i < tabs.length; i++){
     tabs[i].addEventListener('click', toggleMenu)
-}
-
-var toggleMenu = function(){
-	this.classList.toggle('menu-isOpen')
 }
