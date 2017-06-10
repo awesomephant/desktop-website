75847fb50e74dbfbcd446bc386ac1c80fd4c0156
Max Koehler <max@koehler-kn.de>
2017-06-10 16:58:24 +0100

    Add basic sorting function

--- js/dist/app.js
+++ js/dist/app.js
@@ -169,8 +169,28 @@ var setClock = function(){
 setClock();
 window.setInterval(setClock, 60000);var peopleData;
 
+var sortByName = function (el) {
+    peopleData.people.sort(function (a, b) {
+        var nameA = a.Name.toLowerCase(), nameB = b.Name.toLowerCase();
+        if (nameA < nameB) //sort string ascending
+            return -1;
+        if (nameA > nameB)
+            return 1;
+        return 0; //default return value (no sorting)
+    })
+
+    for (var i = 0; i < peopleData.people.length; i++) {
+        let p = peopleData.people[i];
+        console.log(p.Name)
+    }
+}
+
 var initPeopleList = function (el) {
     var people = el.querySelectorAll('.people-list--person')
+    var sortByNameButton = el.querySelector("#js-sortByName");
+    var sortByCourseButton = el.querySelector("#js-sortByCourse");
+    sortByNameButton.addEventListener("click", function () { sortByName(el) })
+    //    sortByCourseButton.addEventListener("click", sortByCourse)
     for (var i = 0; i < people.length; i++) {
         var link = people[i];
         link.addEventListener('click', function () {
@@ -190,7 +210,7 @@ var initPeopleList = function (el) {
     }
 }
 var spawnPeopleWindow = function () {
-    var listTemplate = Handlebars.compile('<ul class="people-list"><li class="people-list--item people-list--header"><span class="people-list--index">#</span><span class="people-list--name">Name</span><span class="people-list--course">Course</span></li>{{#each people}}<li class="people-list--item people-list--person" data-index="{{@index}}"><span class="people-list--index">{{@index}}</span><span class="people-list--name">{{this.Name}}</span><span class="people-list--course">{{this.Course}}</span></a></li>{{/each}}</ul>')
+    var listTemplate = Handlebars.compile('<ul class="people-list"><li class="people-list--item people-list--header"><span class="people-list--index">#</span><span id="js-sortByName" class="people-list--name">Name</span><span id="js-sortByCourse"class="people-list--course">Course</span></li>{{#each people}}<li class="people-list--item people-list--person" data-index="{{@index}}"><span class="people-list--index">{{@index}}</span><span class="people-list--name">{{this.Name}}</span><span class="people-list--course">{{this.Course}}</span></a></li>{{/each}}</ul>')
     var html = listTemplate(peopleData);
     spawnWindow({
         title: 'People',
@@ -207,6 +227,7 @@ fetch('../data/people.json').then(function (response) {
     return response.json();
 }).then(function (data) {
     peopleData = data;
+    spawnPeopleWindow();
 });;var closeMenus = function () {
     var menus = document.querySelectorAll('.desktop-nav-item');
     for (var i = 0; i < menus.length; i++) {
--- js/src/listPeople.js
+++ js/src/listPeople.js
@@ -1,7 +1,27 @@
 var peopleData;
 
+var sortByName = function (el) {
+    peopleData.people.sort(function (a, b) {
+        var nameA = a.Name.toLowerCase(), nameB = b.Name.toLowerCase();
+        if (nameA < nameB) //sort string ascending
+            return -1;
+        if (nameA > nameB)
+            return 1;
+        return 0; //default return value (no sorting)
+    })
+
+    for (var i = 0; i < peopleData.people.length; i++) {
+        let p = peopleData.people[i];
+        console.log(p.Name)
+    }
+}
+
 var initPeopleList = function (el) {
     var people = el.querySelectorAll('.people-list--person')
+    var sortByNameButton = el.querySelector("#js-sortByName");
+    var sortByCourseButton = el.querySelector("#js-sortByCourse");
+    sortByNameButton.addEventListener("click", function () { sortByName(el) })
+    //    sortByCourseButton.addEventListener("click", sortByCourse)
     for (var i = 0; i < people.length; i++) {
         var link = people[i];
         link.addEventListener('click', function () {
@@ -21,7 +41,7 @@ var initPeopleList = function (el) {
     }
 }
 var spawnPeopleWindow = function () {
-    var listTemplate = Handlebars.compile('<ul class="people-list"><li class="people-list--item people-list--header"><span class="people-list--index">#</span><span class="people-list--name">Name</span><span class="people-list--course">Course</span></li>{{#each people}}<li class="people-list--item people-list--person" data-index="{{@index}}"><span class="people-list--index">{{@index}}</span><span class="people-list--name">{{this.Name}}</span><span class="people-list--course">{{this.Course}}</span></a></li>{{/each}}</ul>')
+    var listTemplate = Handlebars.compile('<ul class="people-list"><li class="people-list--item people-list--header"><span class="people-list--index">#</span><span id="js-sortByName" class="people-list--name">Name</span><span id="js-sortByCourse"class="people-list--course">Course</span></li>{{#each people}}<li class="people-list--item people-list--person" data-index="{{@index}}"><span class="people-list--index">{{@index}}</span><span class="people-list--name">{{this.Name}}</span><span class="people-list--course">{{this.Course}}</span></a></li>{{/each}}</ul>')
     var html = listTemplate(peopleData);
     spawnWindow({
         title: 'People',
@@ -38,4 +58,5 @@ fetch('../data/people.json').then(function (response) {
     return response.json();
 }).then(function (data) {
     peopleData = data;
+    spawnPeopleWindow();
 });
